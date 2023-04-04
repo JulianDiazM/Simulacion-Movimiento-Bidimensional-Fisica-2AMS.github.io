ea45b91c452b611d00437943c92ccb0b5a410aa1
Julian Diaz <diazmojulian@gmail.com>
2023-04-04 16:04:40 -0500

    Se creo una animación de un circulo que persigue al puntero, animación la cual será usada posteriormente para determinar el punto de la trayectoria de la circunferencia donde se calculara los diversos componentes del movimiento

--- MCU/MCU.js
+++ MCU/MCU.js
@@ -1,5 +1,5 @@
 //Elementos HTML a JS
-const canvas = document.querySelector(".mcu-canvas"); //Elemento canvas
+const canvas = document.querySelector(".mcu-canvas"); //Elemento canvas donde se dibujaria la trayectoria circular
 const ctx = canvas.getContext("2d");
 const coord_x = document.querySelector(".X");
 const coord_y = document.querySelector(".Y");
@@ -8,9 +8,34 @@ const coord_y = document.querySelector(".Y");
 canvas.height = 400;
 canvas.width = canvas.height; //Escala 1:1
 
+let x = 0;
+let y = 0;
+let imaginary_x = 0;
+let imaginary_y = 0;
+
+
+const dibujarCirculoRelleno = (x, y, radio, startAngle, endAngle, color) => {
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.arc(x, y, radio, startAngle, endAngle, true);
+    ctx.fill();
+    ctx.closePath();
+}
+
 canvas.addEventListener("mousemove", (event) => {
-    let x = event.offsetX;
-    let y = event.offsetY;
+    x = event.offsetX;
+    y = event.offsetY;
     coord_x.innerHTML = `X: ${x}`;
     coord_y.innerHTML = `Y: ${y}`;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (y > (canvas.height / 2)){
+        imaginary_x = x - 30;
+        imaginary_y = y + 30;
+    } else {
+        imaginary_x = x + 30;
+        imaginary_y = y - 30;
+    }
+
+    dibujarCirculoRelleno(imaginary_x, imaginary_y, 10, 0, Math.PI * 2, "#37c83768");
 });
